2e70582582cadc86e14c333d637263c42f5c8088
Ruslan Fadeev <kinrany@yandex.com>
2021-03-13 09:19:33 +0300

    Split src/tests.ts

--- src/tests/apply-json-patch.ts
+++ src/tests/apply-json-patch.ts
@@ -1,14 +1,5 @@
-import { App } from './app';
+import { App } from '../app';
 import { test } from 'tap';
-
-test('/health', async t => {
-  const app = App();
-
-  const response = await app.inject({ method: 'GET', url: '/health' });
-
-  t.same(response.statusCode, 200, 'returns status code 200');
-  t.same(response.body, 'OK\n', 'returns string "OK" that ends with EOL');
-});
 
 test('/apply-json-patch', async t => {
   const app = App();
