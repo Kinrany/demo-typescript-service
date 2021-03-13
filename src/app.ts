6c6c62ecbb1e1a58c20db22300de9f1784bba2f3
Ruslan Fadeev <kinrany@yandex.com>
2021-03-13 08:42:13 +0300

    Add /apply-json-patch

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,61 @@
 import Fastify from 'fastify';
+import { isLeft } from 'fp-ts/lib/Either';
+import * as t from 'io-ts';
+import { Json } from 'io-ts-types'
+import { apply_patch, InvalidPatch, PatchApplyError } from 'jsonpatch';
+
+declare module 'jsonpatch' {
+  export class InvalidPatch extends Error {}
+  export class PatchApplyError extends Error {}
+}
 
 export const App = () => {
   const app = Fastify();
 
-  app.get("/health", async (_req, _res) => 'OK\n');
+  app.get("/health", async () => 'OK\n');
+
+  // Request schema for POST /apply-json-patch
+  const ApplyJsonPatchRequest = t.type({
+    body: t.exact(t.type({
+      document: Json,
+      patch: t.array(Json),
+    })),
+  });
+
+  app.post("/apply-json-patch", async (request, reply) => {
+    const result = ApplyJsonPatchRequest.decode(request);
+    if (isLeft(result)) {
+      return reply.status(400).send(`Invalid request with ${result.left.length} errors.`);
+    }
+    const { document, patch } = result.right.body;
+
+    try {
+      return apply_patch(document, patch);
+    }
+    catch (e: unknown) {
+      if (e instanceof InvalidPatch) {
+        return reply.status(400).send({
+          code: 'InvalidPatch',
+          message: 'Not a valid RFC 6902 JSON patch.',
+          details: e.toString(),
+        });
+      }
+      else if (e instanceof PatchApplyError) {
+        return reply.status(400).send({
+          code: 'PatchApplyError',
+          message: 'Cannot apply the given patch to the given document.',
+          details: e.toString(),
+        });
+      }
+      else {
+        return reply.status(500).send({
+          code: 'InternalError',
+          message: 'Unknown server error',
+          details: (e instanceof Error) ? e.toString() : ''
+        });
+      }
+    }
+  });
 
   return app;
 };
--- src/tests.ts
+++ src/tests.ts
@@ -6,6 +6,35 @@ test('/health', async t => {
 
   const response = await app.inject({ method: 'GET', url: '/health' });
 
-  t.strictEquals(response.statusCode, 200, 'returns status code 200');
-  t.strictEquals(response.body, 'OK\n', 'returns string "OK" that ends with EOL');
+  t.same(response.statusCode, 200, 'returns status code 200');
+  t.same(response.body, 'OK\n', 'returns string "OK" that ends with EOL');
+});
+
+test('/apply-json-patch', async t => {
+  const app = App();
+
+  const document ={
+    data: {
+      value: 100
+    }
+  };
+
+  const patch = [
+    { op: 'add', path: '/data/value', value: 200 }
+  ];
+
+  const expected = {
+    data: {
+      value: 200
+    }
+  };
+
+  const response = await app.inject({
+    method: 'POST',
+    url: '/apply-json-patch',
+    payload: { document, patch }
+  });
+
+  t.same(response.statusCode, 200, 'returns status code 200');
+  t.same(response.json(), expected, 'returns updated document');
 });
