e227cb379bb4ecc84ae418119fe3fb6fcfce6770
Ruslan Fadeev <kinrany@yandex.com>
2021-03-13 06:49:09 +0300

    Add /health endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-console.log("hello world");
+import Fastify from 'fastify';
+
+const app = Fastify();
+
+app.get("/health", async (_req, _res) => 'OK\n');
+
+const PORT = 3000;
+
+app.listen(PORT).catch((error: unknown) => {
+  console.error(error);
+  process.exit(1);
+});
